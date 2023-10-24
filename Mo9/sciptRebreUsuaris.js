ef1dbeb26b1334d964855967a9458203261168c0
C0ural1 <albertcoralm@gmail.com>
2023-10-24 11:42:07 +0200

    insert comanda

--- Mo9/sciptRebreUsuaris.js
+++ Mo9/sciptRebreUsuaris.js
@@ -1,29 +1,63 @@
 const mysql = require('mysql2/promise');
 
-async function obtenerUsuarios() {
-    const connection = await mysql.createConnection({
-        host: "dam.inspedralbes.cat",
-        user: "a22albcormad_botigaG7",
-        password: "botigaG7",
-        database: "a22albcormad_BotigaG7"
-    });
+// Connexio a la base de dades
+const connection = mysql.createPool({
+    host: "dam.inspedralbes.cat",
+    user: "a22albcormad_botigaG7",
+    password: "botigaG7",
+    database: "a22albcormad_BotigaG7"
+});
 
+async function obtenerUsuarios(connection) {
     try {
         const [rows, fields] = await connection.execute('SELECT id_usuari, usuario, passwd FROM Usuari');
         const usuariosJSON = JSON.stringify(rows);
         return usuariosJSON;
     } catch (error) {
-        console.error('Error al obtenir usuaris:', error.message);
+        console.error('Error al obtener usuarios:', error.message);
         throw error;
-    } finally {
-        if (connection) {
-            connection.end();
-        }
     }
 }
 
+async function insertComanda(connection, comandaData) {
+    try {
+        // INSERT
+        const { id_comanda, id_usuari, data_comanda, estat } = comandaData;
+        const [result] = await connection.execute(
+            'INSERT INTO Comanda (id_comanda, id_usuari, data_comanda, estat) VALUES (?, ?, ?, ?)',
+            [id_comanda, id_usuari, data_comanda, estat]
+        );
 
-obtenerUsuarios()
+        // Casos Error
+        if (result.affectedRows === 1) {
+            return 'Comanda insertada correctamente.';
+        } else {
+            return 'No se pudo insertar la comanda.';
+        }
+    } catch (error) {
+        console.error('Error al insertar comanda:', error.message);
+        throw error;
+    }
+}
+
+// Exemple us
+const comandaData = {
+    id_comanda:1,
+    id_usuari: 1,
+    data_comanda: new Date(),
+    estat: true
+};
+
+insertComanda(connection, comandaData)
+    .then(resultado => {
+        console.log(resultado);
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
+
+
+obtenerUsuarios(connection)
     .then(usuariosJSON => {
         console.log('Usuaris:', usuariosJSON);
     })
@@ -31,5 +65,3 @@ obtenerUsuarios()
         console.error('Error:', error.message);
     });
 
-
-module.exports = {obtenerUsuarios}; 
