a02720845a79fa335f6b61876ef6ca5bd36ffcb5
PanDyyG <a22angcamrui@inspedralbes.cat>
2023-11-02 11:33:31 +0100

    Pag principal, falta fotos i alinear bott als laterals

--- Mo7/src/components/PaginaPrincipal.vue
+++ Mo7/src/components/PaginaPrincipal.vue
@@ -1,32 +1,51 @@
 <template>
-    <div class="container">
-      <h1 class="title">Administració Botiga</h1>
-      <div class="buttons">
-        <v-btn @click="$router.push('/GestioComandes')">Gestió de comandes</v-btn><br>
-        <v-btn @click="$router.push('/gestioProductes')">Gestió de Productes</v-btn>
-      </div>
+  <div class="container">
+    <h1 class="title">Administració de la Botiga</h1>
+    <div class="button-container">
+      <v-btn class="custom-button" @click="$router.push('/GestioComandes')">Gestió de Comandes</v-btn>
+      <v-btn class="custom-button" @click="$router.push('/gestioProductes')">Gestió de Productes</v-btn>
     </div>
-  </template>
-  
-  <style>
-  .container {
-    display: flex;
-    flex-direction: column;
-    padding-top: 100px;
-    align-items: center;
-    height: 100vh;
-  }
-  
-  .title {
-    font-size: 3rem;
-    margin-bottom: 2rem;
-  }
-  
-  .buttons {
-    display: flex;
-    flex-direction: column;
-  }
-  .button {
-    margin-bottom: 1rem;
-  }
-  </style>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f4f4f4; /* Cambia el color de fondo a tu preferencia */
+}
+
+.title {
+  font-size: 2.5rem;
+  margin-bottom: 2rem;
+  color: #333; /* Cambia el color del título a tu preferencia */
+}
+
+.button-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.custom-button {
+  margin-bottom: 1.5rem;
+  padding: 1rem 2rem;
+  font-size: 1.2rem;
+  background-color: #66B3FF; /* Cambia el color de fondo del botón a tu preferencia */
+  color: #fff; /* Cambia el color del texto del botón a tu preferencia */
+  border: none;
+  border-radius: 50px; /* Hace que los botones sean redondos */
+  cursor: pointer;
+  display: flex;
+  align-items: center; /* Centra el texto verticalmente en el botón */
+  justify-content: center; /* Centra el texto horizontalmente en el botón */
+  transition: background-color 0.3s ease;
+}
+
+.custom-button:hover {
+  background-color: #0056b3; /* Cambia el color de fondo al pasar el mouse por encima */
+}
+</style>
