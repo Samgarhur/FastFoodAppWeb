6ba598f7c257723532a5b2546b875162bc61394d
alum-01 <a19arnsolrei@inspedralbes.cat>
2023-11-06 09:11:20 +0100

    Implementadas funciones en estadistiques

--- Mo7/src/components/GestioEstadistiques.vue
+++ Mo7/src/components/GestioEstadistiques.vue
@@ -1,84 +1,75 @@
 <template>
+    <button class="my-button-class" onclick="window.location.href = '/';">
+        <img src="./home.png" alt="icono Pag Principal" width="40" height="45">
+    </button>
     <v-container>
-      <v-row justify="center">
-        <v-col v-for="comanda in comandes" :key="comanda.id_comanda" cols="3">
-          <v-card :class="posarColorComandes(comanda.tempsRestant)">
-            <v-card-title>Comanda {{ comanda.id_comanda }}</v-card-title>
-            <v-card-title>Temps: {{ comanda.tempsRestant }} minuts</v-card-title>
-  
-  
-            <v-card-actions>
-              <v-btn @click="veureDetalls(comanda.id_comanda)">Veure Detalls</v-btn>
-              <v-dialog v-model="dialog" max-width="300">
-                <template v-slot:activator="{ on }">
-                  <v-btn class="ma-2" @click="dialog = true">Finalitzar comanda</v-btn>
-                </template>
-                <v-card>
-                  <v-card-title>Confirmación</v-card-title>
-                  <v-card-text>
-                    ¿Estás segur de que la comanda esta finalitzada?
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn @click="marcarComandaFinalitzada(comanda.id_comanda,comanda.tempsRestant)">Sí</v-btn>
-                    <v-btn @click="dialog = false">No</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-card-actions>
-  
-            <v-expand-transition>
-              <v-card v-if="mostrarDetalls && comandaSeleccionada == comanda.id_comanda" class="v-card--reveal"
-                style="height: 100%;">
-                <v-card-text class="pb-0">
-                  <v-list-item>
-                    <v-list-item-content>
-                      <v-list-item-subtitle>Usuari: {{ comanda.nombre_usuario }}</v-list-item-subtitle><br>
-                      Productes ->
-                      <v-list-item-subtitle v-for="producto in comanda.productos" :key="producto.nombre_producto">
-  
-                        {{ producto.nombre_producto }} - Quantitat: {{ producto.quantitat }}
-  
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-  
-                </v-card-text>
-                <v-card-actions class="pt-0">
-                  <v-btn variant="text" color="teal-accent-4" @click="mostrarDetalls = false">
-                    Tancar
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-expand-transition>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>   
-  </template>
-  
+        <v-btn class="ma-2" @click="dialogCrearProducte = true">Recargar estadistiques</v-btn>
+    </v-container>
+    <v-container>
+        <v-card v-for="estadistica in estadistiques">
+            <v-card-title>{{ estadistica.nom }}</v-card-title>
+            <v-img :src="decodeBase64Image(estadistica.foto)" height="150" width="150" cover></v-img>
+        </v-card>
+    </v-container>
+</template>
     
-  <script>
-  
-  import { comandaFinalitzada, getComandasAceptadas } from './communicationsManager';
-  import { socket, state } from './socket';
-  export default {
+<script>
+
+import { comandaFinalitzada, getComandasAceptadas } from './communicationsManager';
+import { socket, state } from './socket';
+export default {
     name: 'GestioEstaditiques',
     data() {
-      return {
-        
-      };
+        return {
+            estadistiques: [],
+
+        };
     },
     methods: {
-      
+        //Funcion para codificar las imagenes del servidor de base64 a una url
+        decodeBase64Image(base64String) {
+            const binaryString = atob(base64String);
+            const bytes = new Uint8Array(binaryString.length);
+            for (let i = 0; i < binaryString.length; i++) {
+                bytes[i] = binaryString.charCodeAt(i);
+            }
+            const blob = new Blob([bytes], { type: "image/jpeg" }); // Ajusta el tipo MIME según tu imagen
+            this.producteEditat.foto = URL.createObjectURL(blob);
+            return URL.createObjectURL(blob);
+        }
+
     },
     created() {
-      
-    },  
-    
-  }
-  </script>
+
+    },
+
+}
+</script>
   
-  <style>
-  
-  </style>
+<style>
+.my-button-class {
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  height: 55px;
+  background-color: #66B3FF;
+  color: #fff;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.3s ease;
+}
+
+.my-button-class img {
+  width: 50px;
+  height: 55px;
+}
+
+.my-button-class:hover {
+  background-color: #0056b3;
+}
+</style>
     
