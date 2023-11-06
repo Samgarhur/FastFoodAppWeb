8b88064722f8bbaab245011a2c4b2be7ee7bd2d4
PanDyyG <a22angcamrui@inspedralbes.cat>
2023-11-06 12:41:51 +0100

    DinersPerDia, TempsFinalitzarComanda, ProductesPreu phyton

--- Mo10/produirInfo.py
+++ Mo10/produirInfo.py
@@ -1,15 +1,10 @@
-#pedidos por dia xDias, yPedidos json=comandes
-#dinero por dia xDias, yDinero json=join productes, comandes, comandaProd
-#tiempo finalizar xidComanda, yTIempo--Recta regresion json=comanda
-#productos/precio xnumCompras yPrecio json=productes
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from datetime import date
 import sys
 import json
-
+from sklearn.linear_model import LinearRegression
 
 avui = date.today();
 terminacio=avui ,'.jpeg';
@@ -84,6 +79,70 @@ def ComandesXdia (info2): #pedidos por dia xDias, yPedidos json=comandes
     ax.set_xlabel('Dies')  
     ax.set_ylabel('Comandes')
     plt.savefig('ComandesAlDia'+terminacio)
+    plt.close('all')
+
+def DinersPerDia(info2): #dinero por dia xDias, yDinero json=join productes, comandes, comandaProd
+    # Llegir les dades de les comandes i els productes
+    comandes_df = pd.read_json(info2, lines=True)
+    productes_df = pd.read_json(info2, lines=True)
+
+    # Unir les comandes amb els productes fent servir 'comandaProd' i 'idComanda'
+    dades_fusionades = pd.merge(comandes_df, productes_df, left_on='idComanda', right_on='idComanda')
+
+    # Calcular els diners totals per dia
+    diners_per_dia = dades_fusionades.groupby('dies')['preu'].sum()
+
+    x_values = diners_per_dia.index
+    y_values = diners_per_dia.values
+
+    plt.bar(x_values, y_values)
+    plt.title('Diners per Dia')
+    ax = plt.subplot()
+    ax.set_xticks(x_values)
+    ax.set_xticklabels(x_values)
+    ax.set_xlabel('Dia')
+    ax.set_ylabel('Diners')
+    plt.savefig('DinersPerDia' + terminacio)
+    plt.close('all')
+
+def TempsFinalitzarComanda(info2): #tiempo finalizar xidComanda, yTIempo--Recta regresion json=comanda
+    # Llegir les dades de les comandes
+    comandes_df = pd.read_json(info2, lines=True)
+
+    # Filtrar les comandes amb 'idComanda' corresponent
+    id_comanda = info2[1]
+    comanda_df = comandes_df[comandes_df['idComanda'] == id_comanda]
+
+    x_values = comanda_df['idComanda']
+    y_values = comanda_df['TIempo']  # Suposem que aquesta és la columna que conté el temps
+
+    # Realitzar una regressió lineal
+    model = LinearRegression()
+    model.fit(x_values.values.reshape(-1, 1), y_values.values.reshape(-1, 1))
+
+    # Predir el temps per finalitzar
+    y_pred = model.predict(x_values.values.reshape(-1, 1))
+
+    plt.scatter(x_values, y_values, label='Dades reals')
+    plt.plot(x_values, y_pred, color='red', label='Regressió lineal')
+    plt.title('Temps per Finalitzar Comanda')
+    plt.xlabel('ID de Comanda')
+    plt.ylabel('Temps (minuts)')
+    plt.legend()
+    plt.savefig('TempsFinalitzarComanda' + terminacio)
+    plt.close('all')
+
+def ProductesPreu(info2): #productos/precio xnumCompras yPrecio json=productes
+    df = pd.read_json(info2, lines=True)
+
+    x_values = df['numCompra']
+    y_values = df['Preu']
+
+    plt.scatter(x_values, y_values)
+    plt.title('Productes/Preu')
+    plt.xlabel('Nombre de Compres')
+    plt.ylabel('Preu')
+    plt.savefig('ProductesPreu' + terminacio)
     plt.close('all')
 
 def cridarFuncions(info2):
